cd717155bbb78ba0aedd6f27550d08dc91a7258c
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2024-09-17 08:00:24 +0300

    exercise 3: implement functionality to add new blogs

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -3,13 +3,32 @@ import Blog from './Blog'
 import blogService from '../services/blogs'
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+        const newBlog = await blogService.create({
+            title, author, url
+        })
+        setBlogs(blogs.concat(newBlog))
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+    }
+    catch (exception) {
+        console.log('failed to create blog: ', exception)
+    }
+  }
 
   return (
     <div>
@@ -17,6 +36,13 @@ const Blogs = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      <h2>create new</h2>
+        <form onSubmit={handleSubmit}>
+            <div>title: <input type="text" value={title} onChange={({ target }) => setTitle(target.value)}/></div>
+            <div>author: <input type="text" value={author} onChange={({ target }) => setAuthor(target.value)}/></div>
+            <div>url: <input type="text" value={url} onChange={({ target }) => setUrl(target.value)}/></div>
+            <button type="submit">create</button>
+        </form>
     </div>
   )
 }
