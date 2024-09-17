0b61511f74ee567b1c436c4d87b3edfd9b6350a5
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2024-09-17 21:29:16 +0300

    exercises 8-11: implement like + delete functionalities

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import Toggable from "./Toggable"
+import blogService from '../services/blogs'
 
-const Blog = ({ blog }) => {
+const Blog = ({ blogs, setBlogs, blog, notify }) => {
     const blogStyle = {
         paddingTop: 10,
         paddingLeft: 2,
@@ -9,14 +10,37 @@ const Blog = ({ blog }) => {
         marginBottom: 5
     }
 
+    const like = async () => {
+        try {
+            const updatedBlog = { ...blog, likes: blog.likes + 1 }
+            await blogService.update(blog.id, updatedBlog)
+            setBlogs(blogs.map(b => b.id === blog.id ? updatedBlog : b))
+        }
+        catch (exception) {
+            notify(`unable to like blog: ${exception.message}`, 'error')
+        }
+    }
+
+    const remove = async () => {
+        try {
+            window.confirm(`remove blog ${blog.title} by ${blog.author}`)
+            await blogService.remove(blog.id)
+            setBlogs(blogs.filter(b => b.id !== blog.id))
+        }
+        catch (exception) {
+            notify(`unable to remove blog: ${exception.message}`, 'error')
+        }
+    }
+
     return (
         <div style={blogStyle}>
             <h3>{blog.title} {blog.author} </h3>
             <Toggable buttonLabel="view">
                 <p>
                     {blog.url} <br />
-                    likes {blog.likes} <button>like</button> <br />
-                    {blog.user.username}
+                    likes {blog.likes} <button onClick={like}>like</button> <br />
+                    {blog.user.username} <br />
+                    <button onClick={remove}>remove</button>
                 </p>
             </Toggable>
         </div>  
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -13,14 +13,18 @@ const Blogs = ({ notify }) => {
       )
     }, [])
 
+    useEffect(() => {
+        console.log('sorting blogs')
+        const newBlogs = blogs
+        setBlogs(newBlogs.sort((a, b) => b.likes - a.likes + 1))
+    }, [blogs])
+
     const blogFormRef = useRef()
 
     return (
       <div>
           <h2>blogs</h2>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-          )}
+          { blogs.map(blog => <Blog key={blog.id} blog={blog} notify={notify} blogs={blogs} setBlogs={setBlogs}/>) }
           <Toggable buttonLabel="create new blog" ref={blogFormRef}>
               <BlogForm notify={notify} setBlogs={setBlogs} blogs={blogs} blogFormRef={blogFormRef} />
           </Toggable>
